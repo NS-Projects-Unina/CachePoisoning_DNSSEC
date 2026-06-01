fc20c590309ec09e15c2b453e409b6fb77665644
Pasquale Paolo Silvenni <101285231+PasqualePaoloSilvenni@users.noreply.github.com>
2026-06-01 23:11:24 +0200

    Add Python setup - DNS security project

---
+++ setup.py
@@ -0,0 +1,7 @@
+"""
+DNS Security: Cache Poisoning Analysis & DNSSEC Implementation
+A comprehensive study of DNS vulnerabilities and hardening techniques.
+"""
+
+__version__ = "1.0"
+__author__ = "Pasquale Paolo Silvenni"
